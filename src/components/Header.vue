5eb4b0b2bdae1e3c5963b4ce9db75e28ab5fe44a
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-10-25 16:26:08 +0200

    class tailwind importer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,43 +1,39 @@
 <template>
-   <nav>
-    <h4 class="text-xl">
-      <Bars3Icon class="inline-block h-5 w-5 text-blue-500" />
-      menu <code class="font-mono">/src/App.vue</code>
-    </h4>
-    <LoginLogout/>
+   <div>
     <ul>
+    
       <li>
         <router-link class="text-red-600 underline" to="/">
-          lien vers
+          
           <code class="font-mono">TIK TAK</code>
         </router-link>
       </li>
       <li>
  <router-link class="text-red-600 underline" to="/personnalisation">
- lien vers
- <code class="font-mono">/src/pages/PersonnalisationView.vue</code>
+
+ <code class="font-mono">Personnalisation</code>
  </router-link>
  </li>
  <li>
  <router-link class="text-red-600 underline" to="/Galerie">
- lien vers
- <code class="font-mono">/src/pages/GalerieView.vue</code>
+
+ <code class="font-mono">Galerie</code>
  </router-link>
  </li>
  <li>
         <router-link class="text-red-600 underline" to="/Contact">
-          lien vers
-          <code class="font-mono">/src/pages/ContactView.vue</code>
+         
+          <code class="font-mono">Contact</code>
         </router-link>
       </li>
        <li>
         <router-link class="text-red-600 underline" to="/Seconnecter">
-          lien vers
-          <code class="font-mono">/src/pages/Seconnecter</code>
+        
+          <code class="font-mono">Se connecter</code>
         </router-link>
       </li>
     </ul>
-  </nav>
+</div>
 
   <!-- Affiche les pages -->
   <router-view class="m-2 border-2 p-2" /> 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,34 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        'bleu': '#'
-      },
-      fontFamily: {
 
+      "colorStyles": {
+        "cadre": "#7E9BA1",
+        "blanc": "#FFFFFF",
+        "orange": "#EE4F2C",
+        "gris_clair": "#D9D9D9",
+        "gris_transparant": "#E3E3E3",
+        "noir": "#000000",
+        "marron_clair": "#A79393",
+        "Fond": "#ECF2F3",
+        "gris_fonce": "#565A5F",
+        "vert1": "#7E9BA1",
+        "vert2": "#85B4A6",
+        "vert3": "#91AD80",
+        "rose1": "#A482AF",
+        "jaune1": "#C0B598",
+        "rouge_pale": "#DE8484"
       },
-    },
+      "fontStyles": {
+        "Titreh1": "font-JosefinSans font-[700] text-[84px] ",
+        "text": "font-JosefinSans font-[400] text-[24px] ",
+        "boutonmenu": "font-JosefinSans font-[400] text-[24px] ",
+        "text_mini_menu": "font-JosefinSans font-[300] text-[24px] ",
+        "Titreh2": "font-JosefinSans font-[500] text-[80px] ",
+        "boutonlien": "font-JosefinSans font-[400] text-[24px] ",
+        "Titreh3": "font-JosefinSans font-[500] text-[41px] "
+      }
+    }
   },
   plugins: [
     require("@tailwindcss/typography"),
