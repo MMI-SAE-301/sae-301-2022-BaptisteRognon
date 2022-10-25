a1ca2471de4498df83311a3b8d5314cbb77ad65f
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-10-25 16:42:42 +0200

    changer couleur bouton nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,33 +1,33 @@
 <template>
-   <div>
+   <div class="flex">
     <ul>
     
       <li>
-        <router-link class="text-red-600 underline" to="/">
+        <router-link class="text-noir " to="/">
           
           <code class="font-mono">TIK TAK</code>
         </router-link>
       </li>
       <li>
- <router-link class="text-red-600 underline" to="/personnalisation">
+ <router-link class="text-noir " to="/personnalisation">
 
- <code class="font-mono">Personnalisation</code>
+ <code class="font-boutonmenu">Personnalisation</code>
  </router-link>
  </li>
  <li>
- <router-link class="text-red-600 underline" to="/Galerie">
+ <router-link class="text-noir font- " to="/Galerie">
 
  <code class="font-mono">Galerie</code>
  </router-link>
  </li>
  <li>
-        <router-link class="text-red-600 underline" to="/Contact">
+        <router-link class="text-noir " to="/Contact">
          
           <code class="font-mono">Contact</code>
         </router-link>
       </li>
        <li>
-        <router-link class="text-red-600 underline" to="/Seconnecter">
+        <router-link class="text-noir " to="/Seconnecter">
         
           <code class="font-mono">Se connecter</code>
         </router-link>
