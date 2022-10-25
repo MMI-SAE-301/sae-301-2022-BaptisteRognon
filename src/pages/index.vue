b8fd77ab63caff7d257c9ecacbf739db403430be
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-10-25 15:14:58 +0200

    importation img

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,15 @@
 <template>
+  <div class="grid ">
+    <h1 class="text-2xl">Mes TIK TAK</h1>
+    <h1 class="text-2xl">La montre des hauts</h1>
+  </div>
+
+  <div class="">
+    <h1 class="text-2xl">
+      Bonjour<code class="font-mono text-base">/src/pages/index.vue</code>
+    </h1>
+  </div>
+
   <div class="">
     <h1 class="text-2xl">
       Bonjour<code class="font-mono text-base">/src/pages/index.vue</code>
