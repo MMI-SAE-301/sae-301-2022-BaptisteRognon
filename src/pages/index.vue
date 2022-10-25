97be6f7767f66919ca7ceb293ff93bef311ce720
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-10-25 14:59:10 +0200

    création des pages

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <section>
+  <div class="">
     <h1 class="text-2xl">
       Bonjour<code class="font-mono text-base">/src/pages/index.vue</code>
     </h1>
-  </section>
+  </div>
 </template>
