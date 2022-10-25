6675473a3981f126d20be6e17e2afef12ba8e710
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-10-25 14:46:29 +0200

    page loginlogout créer et lier authentification

---
+++ src/components/LoginLogout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { supabase, user } from '../supabase';
+</script>
+<template>
+<div>
+ <button v-if="user"
+@pointerdown="supabase.auth.signOut()">
+ Se déconnecter ({{user.email}})
+ </button>
+ <button v-else
+@pointerdown="supabase.auth.signIn({provider: 'google'})">
+ Se connecter avec Google
+ </button>
+  <button v-else
+@pointerdown="supabase.auth.signIn({provider: 'facebook'})">
+ Se connecter avec Facebook
+ </button>
+</div>
+</template> 
