256c3e32ec60d25006b5f49b5446795838991685
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-10-26 14:53:41 +0200

    modification app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <Header/>
-  
+  <Footer/>
 </template>
 
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
