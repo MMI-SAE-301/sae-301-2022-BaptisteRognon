2c3f15958f58c650b98a71899d0652f37741c31a
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-10-26 09:40:42 +0200

    modification index.html

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,53 @@
 <template class="background-black">
   <div name="page d'accueil" >
-    <h1 class="text-2xl">Mes TIK TAK</h1>
-    <h1 class="text-2xl">La montre des hauts</h1>
+  <div name="entête" class="p-5">
+    <div>
+    <h1 class="text-2xl font-text_mini_menu">Mes TIK TAK</h1>
+    <h1 class="text-2xl font-text_mini_menu">La montre des hauts</h1>
   </div>
 
-  <div class="">
+  <div class="p-5 font-text_mini_menu">
     <p class="">Un luxe classique,</p>
     <P>pour vous mettre en valeur.</p>
     <br/>
-    <P>Pour cela, il faut mettre le prix</p>
-      
+    <P>Pour cela, il faut mettre le prix</p>  
   </div>
 
-  <div class="">
-    <h1 class="text-2xl">
-      Bonjour<code class="font-mono text-base">/src/pages/index.vue</code>
-    </h1>
+    <div class="">
+      <h1 class="text-2xl">
+        <code class="font-mono text-base">Achetez Maintenant</code>
+      </h1>
+    </div>
+  </div>
+
+  <div name="article1" class="grid grid-cols-3">
+    <div class="justify-center">
+      <p>Galerie</p>
+      <p>Personnalisation</p>
+      <p>Contact</p>
+      <p>Avis</p>
+    </div>
+
+    <div class="justify-center">
+    <p>Votre TIK TAK</p>
+    <p>Une Merveille</p>
+    <p>Le nom de la marque de montre est TiK•TaK, sa philosophie est un style raffiné, minimaliste et épuré. L’identité visuelle devra être en adéquation avec la marque.</p>
+    <code>Allez voir ! </code>
+    <img src="../../img/cercleo.jpg" alt="image de montre"/>
+    </div>
+
+    <div class="justify-center">
+      <img src="../../public/img/tiktakrectanlge.png"/>
+    </div>
+  </div>
+
+  <div name="article2">
+    <img src="../../public/img/image1.jpg"/>
+    <img src="@/img/image2.jpg"/>
+  </div>
+
+
+
+
   </div>
 </template>
