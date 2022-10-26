f68d92c6c9d45a71e53f6c82765d41a115003e03
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-10-26 14:54:04 +0200

    commencement du footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="">
+    <div class="bg-gris_fonce  flex justify-center p-10 gap-x-5" >
+<img src="../../public/icon/facebook.png">
+<img src="../../public/icon/twitter.png">
 
     </div>
 </template>
