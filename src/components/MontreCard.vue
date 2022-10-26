65953bb8f100aef156a23660d1082219af8f6b68
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-10-26 15:52:01 +0200

    mise à jour de la card montre

--- src/components/MontreCard.vue
+++ src/components/MontreCard.vue
@@ -1,20 +1,138 @@
 <template>
-    <div class="m-3 bg-red-500" width="20px" height="20px" >
-        <img src="../../public/img/Avatar1.jpg" alt="utilisateur" >       
-         <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
-            <h3 class="text-2xl text-gray-900">{{ titre }}</h3>
+    <div class="m-3 grid grid-cols-3 bg-gris_clair" width="20px" height="20px" >
+        
+        
+       
+    <div class="bg-red w-64">
+        <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
+            <h3 class="text-2xl ">{{ titre }}</h3>
             <p>Rien à dire, la personnalisation des TIK TAK est vraiment complête ! </p>
-            <div class="flex justify-evenly">
-            <a class="bg-white m-3">Profile</a>
-            <a class="bg-white m-3">Ajouter un avis</a>
-            </div>
+            
         </figcaption>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 380.41 711">
+       
+        <g id="bracelet"><path  d="M550.41,73.36S602.58,53.07,649,54.52s95.65,15.94,95.65,15.94-29,271-26.09,349.28S746.06,727,746.06,727s-47.83,21.73-92.76,18.84-97.1-17.4-97.1-17.4S580.09,489.3,580.09,395.1,550.41,73.36,550.41,73.36Z" 
+            fill="bg-blue-600" stroke="#FFFFFF" stroke-width="0.5px" stroke-miterlimit="10" class="bracelet" transform="translate(-459.17 -45.58)"/>
+            <path d="M509.83,199.45S549,127,544.61,73.36c0,0,17.39-27.53,102.9-27.53S757.65,64.67,757.65,64.67s14.8,110.14,35.09,130.43c0,0-96,18.84-148.13,18.84S509.83,199.45,509.83,199.45Z"
+            fill="bg-blue-600" stroke="violet" stroke-width="0.5px" stroke-miterlimit="10" class="bracelet" transform="translate(-459.17 -45.58)"/>
+            <path d="M795.14,607.79s-41.87,70.92-39.57,124.67c0,0-18.43,26.85-103.87,23.59s-109.35-23-109.35-23S531.76,622.4,512.26,601.35c0,0,96.61-15.17,148.74-13.18S795.14,607.79,795.14,607.79Z"
+            fill="bg-blue-600" stroke="jaune" stroke-width="0.5px" stroke-miterlimit="10" class="bracelet" transform="translate(-459.17 -45.58)"/>
+        </g>
+        <g id="boitier" fill="bg-blue-200" stroke="black" stroke-width="0.5px" stroke-miterlimit="10" class="boitier">
+            <rect class="boitier" x="0.25" y="130.63" width="379.91" height="453.62" rx="80.8"/>
+            
+        </g>
+        <g id="ecran" fill="bg-blue-800" stroke="black" stroke-width="0.5px" stroke-miterlimit="10" class="ecran">
+            <rect class="boitier" x="38.43" y="172.53" width="303.55" height="369.83" rx="65.21"/>
+            
+        </g>
+        <g>
+            <text class="cls-3" transform="translate(59.96 329.78)">1O:1O</text>
+            <text class="cls-4" transform="translate(112.05 222.03)">
+                <tspan xml:space="preserve">Lun.  24 oct.</tspan>
+            </text><text class="cls-5" transform="translate(137.72 511.45)">TiK•TaK</text>
+        </g>
+        </svg>
+        <div class="flex bg-black justify-evenly">
+            <a class= "text-white m-3">Panier</a>
+            <a class="text-white m-3">Achetez !</a>
+            </div>
+        </div>
+
+
+
+
+        <div class="bg-red w-64">
+        <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
+            <h3 class="text-2xl ">{{ titre }}</h3>
+            <p>Rien à dire, la personnalisation des TIK TAK est vraiment complête ! </p>
+            
+        </figcaption>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 380.41 711">
+       
+        <g id="bracelet"><path  d="M550.41,73.36S602.58,53.07,649,54.52s95.65,15.94,95.65,15.94-29,271-26.09,349.28S746.06,727,746.06,727s-47.83,21.73-92.76,18.84-97.1-17.4-97.1-17.4S580.09,489.3,580.09,395.1,550.41,73.36,550.41,73.36Z" 
+            fill="bg-blue-600" stroke="#FFFFFF" stroke-width="0.5px" stroke-miterlimit="10" class="bracelet" transform="translate(-459.17 -45.58)"/>
+            <path d="M509.83,199.45S549,127,544.61,73.36c0,0,17.39-27.53,102.9-27.53S757.65,64.67,757.65,64.67s14.8,110.14,35.09,130.43c0,0-96,18.84-148.13,18.84S509.83,199.45,509.83,199.45Z"
+            fill="bg-blue-600" stroke="violet" stroke-width="0.5px" stroke-miterlimit="10" class="bracelet" transform="translate(-459.17 -45.58)"/>
+            <path d="M795.14,607.79s-41.87,70.92-39.57,124.67c0,0-18.43,26.85-103.87,23.59s-109.35-23-109.35-23S531.76,622.4,512.26,601.35c0,0,96.61-15.17,148.74-13.18S795.14,607.79,795.14,607.79Z"
+            fill="bg-blue-600" stroke="jaune" stroke-width="0.5px" stroke-miterlimit="10" class="bracelet" transform="translate(-459.17 -45.58)"/>
+        </g>
+        <g id="boitier" fill="bg-blue-200" stroke="black" stroke-width="0.5px" stroke-miterlimit="10" class="boitier">
+            <rect class="boitier" x="0.25" y="130.63" width="379.91" height="453.62" rx="80.8"/>
+            
+        </g>
+        <g id="ecran" fill="bg-blue-800" stroke="black" stroke-width="0.5px" stroke-miterlimit="10" class="ecran">
+            <rect class="boitier" x="38.43" y="172.53" width="303.55" height="369.83" rx="65.21"/>
+            
+        </g>
+        <g>
+            <text class="cls-3" transform="translate(59.96 329.78)">1O:1O</text>
+            <text class="cls-4" transform="translate(112.05 222.03)">
+                <tspan xml:space="preserve">Lun.  24 oct.</tspan>
+            </text><text class="cls-5" transform="translate(137.72 511.45)">TiK•TaK</text>
+        </g>
+        </svg>
+        <div class="flex bg-black justify-evenly">
+            <a class= "text-white m-3">Panier</a>
+            <a class="text-white m-3">Achetez !</a>
+            </div>
+        </div>
+
+
+
+        <div class="bg-red w-64">
+        <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
+            <h3 class="text-2xl ">{{ titre }}</h3>
+            <p>Rien à dire, la personnalisation des TIK TAK est vraiment complête ! </p>
+            
+        </figcaption>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 380.41 711">
+       
+        <g id="bracelet"><path  d="M550.41,73.36S602.58,53.07,649,54.52s95.65,15.94,95.65,15.94-29,271-26.09,349.28S746.06,727,746.06,727s-47.83,21.73-92.76,18.84-97.1-17.4-97.1-17.4S580.09,489.3,580.09,395.1,550.41,73.36,550.41,73.36Z" 
+            fill="bg-blue-600" stroke="#FFFFFF" stroke-width="0.5px" stroke-miterlimit="10" class="bracelet" transform="translate(-459.17 -45.58)"/>
+            <path d="M509.83,199.45S549,127,544.61,73.36c0,0,17.39-27.53,102.9-27.53S757.65,64.67,757.65,64.67s14.8,110.14,35.09,130.43c0,0-96,18.84-148.13,18.84S509.83,199.45,509.83,199.45Z"
+            fill="bg-blue-600" stroke="violet" stroke-width="0.5px" stroke-miterlimit="10" class="bracelet" transform="translate(-459.17 -45.58)"/>
+            <path d="M795.14,607.79s-41.87,70.92-39.57,124.67c0,0-18.43,26.85-103.87,23.59s-109.35-23-109.35-23S531.76,622.4,512.26,601.35c0,0,96.61-15.17,148.74-13.18S795.14,607.79,795.14,607.79Z"
+            fill="bg-blue-600" stroke="jaune" stroke-width="0.5px" stroke-miterlimit="10" class="bracelet" transform="translate(-459.17 -45.58)"/>
+        </g>
+        <g id="boitier" fill="bg-blue-200" stroke="black" stroke-width="0.5px" stroke-miterlimit="10" class="boitier">
+            <rect class="boitier" x="0.25" y="130.63" width="379.91" height="453.62" rx="80.8"/>
+            
+        </g>
+        <g id="ecran" fill="bg-blue-800" stroke="black" stroke-width="0.5px" stroke-miterlimit="10" class="ecran">
+            <rect class="boitier" x="38.43" y="172.53" width="303.55" height="369.83" rx="65.21"/>
+            
+        </g>
+        <g>
+            <text class="cls-3" transform="translate(59.96 329.78)">1O:1O</text>
+            <text class="cls-4" transform="translate(112.05 222.03)">
+                <tspan xml:space="preserve">Lun.  24 oct.</tspan>
+            </text><text class="cls-5" transform="translate(137.72 511.45)">TiK•TaK</text>
+        </g>
+        </svg>
+        <div class="flex bg-black justify-evenly">
+            <a class= "text-white m-3">Panier</a>
+            <a class="text-white m-3">Achetez !</a>
+            </div>
+        </div>
+
+
+
+
     </div>
 </template>
 
-<script setup >
 
-import Montre from 
-   
+  
+
+<script setup lang="ts">
+
+
+
+defineProps({
+    titre: {type:String, default:'Paul Moulin'},
+    Text: {type:String, default:'Très satisfait du service. Livré rapidement en moin de 24h. Après, normal pour un marque de luxe '},
+    
+})
 
 </script>
