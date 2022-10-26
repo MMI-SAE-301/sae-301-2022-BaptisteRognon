c09c98f2bdac048d5c9c7c4d8509de8dfae366b6
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-10-26 14:53:54 +0200

    modification composant avis

--- src/components/AvisCard.vue
+++ src/components/AvisCard.vue
@@ -1,5 +1,6 @@
 <template> 
-    <div class="m-3 bg-red-500"  width="20px" height="20px" >
+<div class="justify-center grid grid-cols-3">
+    <div class="m-3 bg-red-500" width="20px" height="20px" >
         <img src="../../public/img/Avatar1.jpg" alt="utilisateur" >       
          <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
             <h3 class="text-2xl text-gray-900">{{ titre }}</h3>
@@ -9,6 +10,67 @@
             <a class="bg-white m-3">Ajouter un avis</a>
             </div>
         </figcaption>
+    </div>
+
+    <div class="m-3 bg-red-500" width="20px" height="20px" >
+        <img src="../../public/img/Avatar1.jpg" alt="utilisateur" >       
+         <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
+            <h3 class="text-2xl text-gray-900">{{ titre }}</h3>
+            <p>Rien à dire, la personnalisation des TIK TAK est vraiment complête ! </p>
+            <div class="flex justify-evenly">
+            <a class="bg-white m-3">Profile</a>
+            <a class="bg-white m-3">Ajouter un avis</a>
+            </div>
+        </figcaption>
+    </div>
+
+    <div class="m-3 bg-red-500" width="20px" height="20px" >
+        <img src="../../public/img/Avatar1.jpg" alt="utilisateur" >       
+         <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
+            <h3 class="text-2xl text-gray-900">{{ titre }}</h3>
+            <p>Rien à dire, la personnalisation des TIK TAK est vraiment complête ! </p>
+            <div class="flex justify-evenly">
+            <a class="bg-white m-3">Profile</a>
+            <a class="bg-white m-3">Ajouter un avis</a>
+            </div>
+        </figcaption>
+    </div>
+
+    <div class="m-3 bg-red-500" width="20px" height="20px" >
+        <img src="../../public/img/Avatar1.jpg" alt="utilisateur" >       
+         <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
+            <h3 class="text-2xl text-gray-900">{{ titre }}</h3>
+            <p>Rien à dire, la personnalisation des TIK TAK est vraiment complête ! </p>
+            <div class="flex justify-evenly">
+            <a class="bg-white m-3">Profile</a>
+            <a class="bg-white m-3">Ajouter un avis</a>
+            </div>
+        </figcaption>
+    </div>
+
+    <div class="m-3 bg-red-500" width="20px" height="20px" >
+        <img src="../../public/img/Avatar1.jpg" alt="utilisateur" >       
+         <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
+            <h3 class="text-2xl text-gray-900">{{ titre }}</h3>
+            <p>Rien à dire, la personnalisation des TIK TAK est vraiment complête ! </p>
+            <div class="flex justify-evenly">
+            <a class="bg-white m-3">Profile</a>
+            <a class="bg-white m-3">Ajouter un avis</a>
+            </div>
+        </figcaption>
+    </div>
+
+    <div class="m-3 bg-red-500" width="20px" height="20px" >
+        <img src="../../public/img/Avatar1.jpg" alt="utilisateur" >       
+         <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
+            <h3 class="text-2xl text-gray-900">{{ titre }}</h3>
+            <p>Rien à dire, la personnalisation des TIK TAK est vraiment complête ! </p>
+            <div class="flex justify-evenly">
+            <a class="bg-white m-3">Profile</a>
+            <a class="bg-white m-3">Ajouter un avis</a>
+            </div>
+        </figcaption>
+    </div>
     </div>
 </template>
 
