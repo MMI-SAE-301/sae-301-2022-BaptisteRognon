aa94c56f3cf064dc9b02db76d2de53a16b6586ee
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-10-26 10:55:02 +0200

    En cours création de la page personnalisation

---
+++ src/pages/personnalisation.vue
@@ -0,0 +1,15 @@
+
+<template>
+        
+
+ <FormMateriaux />
+ 
+ </template>
+ <script setup lang="ts">
+
+import FormMateriaux from "../components/FormMateriaux.vue";
+import type { MontreSvg } from "@/types-svg";
+import { ref } from "vue";
+
+    const Montre = ref<MontreSvg>({})
+ </script>
